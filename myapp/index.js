5ec20dde449885118465a8a8095d0b4c2107e18b
MarCarrot <marmar18@live.com>
2021-04-09 16:45:45 -0500

    Added empty pages for login, registration, and non-existing pages

--- myapp/index.js
+++ myapp/index.js
@@ -18,32 +18,47 @@ app.set('view engine', 'ejs');
 
 // route views
 app.get('/', (req, res) => {
- res.render('home.ejs')
+    const pageName = "Home";
+    res.render('home.ejs', {pageInfo : pageName})
 
 })
 
 app.get('/restaurants', (req, res) => {
-    res.render('restaurants.ejs')
+    const pageName = "Restaurants";
+    res.render('restaurants.ejs', { pageInfo: pageName })
 })
 
-app.get('/recipies', (req, res) => {
-    res.render('recipies.ejs')
+app.get('/recipes', (req, res) => {
+    const pageName = "Recipes";
+    res.render('recipes.ejs', { pageInfo: pageName })
 })
+
+app.get('/login', (req, res) => {
+    const pageName = "Login";
+    res.render('login.ejs', { pageInfo: pageName })
+})
+
+app.get('/register', (req, res) => {
+    const pageName = "Register";
+    res.render('register.ejs', { pageInfo: pageName })
+})
+
 
 // For any undefined pages, handle here
 app.get('*', (req, res) => {
-    res.send('Page not found :(')
-   })
+    const pageName = "Unkown";
+    res.render('unknown.ejs', { pageInfo: pageName })
+})
 
 app.get('/users', db.getUsers)
 
 app.get('/user/:accountId', (req, res) => {
-res.send('User: ' + req.params.accountId)
+    res.send('User: ' + req.params.accountId)
 });
-   
+
 
 app.listen(port, () => {
- console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
 
 // This app starts a server and listens on port 3000 for connections.
