3dd9f2c2c7749317fe7f2981fbab614c33ddcd54
MarCarrot <60483997+MarCarrot@users.noreply.github.com>
2021-04-17 22:26:35 -0500

    Added functionality to "show password"

--- myapp/public/js/initialize.js
+++ myapp/public/js/initialize.js
@@ -1,3 +1,10 @@
+
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+$(document).ready(function(){
+  $("#showPassword").click(function(){
+    $(this).is(':checked') ? $('#inputPassword').attr('type', 'text') : $('#inputPassword').attr('type', 'password');
+  });
+});  
