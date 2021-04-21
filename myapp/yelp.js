d21b336ce7f4001534700677f54a9a9fa47f65bc
nikhitha06 <nikhitha06@tamu.edu>
2021-04-20 22:35:12 -0500

    modified

--- myapp/yelp.js
+++ myapp/yelp.js
@@ -3,13 +3,28 @@ const axios = require('axios').default
 const baseUrl = 'https://api.yelp.com/v3/'
 const auth = 'Bearer eYND9foowwisuxbuMvW1bUGApWxytHH3rkC2TfD0equwvbjUns0vTKiwh6ewOp6as2PWNCBv2l7AIdfxtK88BILm1yqDOIb1YKr6TlXAg9O4DSQaUISWlb8mI9lTYHYx'
 
+function toMiles(meters) {
+    return (meters)/1609;
+  }
+
 // async function searchRestaurant(cuisine, item, distance = 10000)
 // {
-
 // }
-
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
 async function testRestaurant()
 {
+    console.log('calling');
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+    
+
+    try{
     var trimmedRes = {
         "name": "",
         "url": "",
@@ -24,7 +39,7 @@ async function testRestaurant()
           term: 'thai food',
           location: 'Texas A&M University',
           limit: 3,
-          radius: 2000
+          radius: toMiles(3000)
         }
     });
     res = res.data;
@@ -33,10 +48,12 @@ async function testRestaurant()
     trimmedRes.address = res.businesses[0].location.address1;
     trimmedRes.city = res.businesses[0].location.city;
     console.log(trimmedRes)
-
+    }
+    catch{throw new Error('Failed');}
 }
 
 module.exports = {
     testRestaurant
     // searchRestaurant
 }
+
