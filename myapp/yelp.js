3dfc992cdc99b742837cdb7c19fe237bf886aff4
Kyle Mrosko <kylemrosko@gmail.com>
2021-04-21 19:14:06 -0500

    kyle test 1

--- myapp/yelp.js
+++ myapp/yelp.js
@@ -19,39 +19,37 @@ function resolveAfter2Seconds() {
       }, 2000);
     });
   }
+  
 async function testRestaurant()
 {
   console.log('calling');
   const result = await resolveAfter2Seconds();
-  console.log(result);
+  console.log('here');
+     
+  var trimmedRes = {
+      "name": "",
+      "url": "",
+      "address": "",
+      "city": ""
+  }
+  let res = await axios.get(baseUrl + 'businesses/search', {
+      headers: {
+          Authorization: auth
+      },
+      params: {
+        term: 'thai food',
+        location: 'Texas A&M University',
+        limit: limit,
+        radius: dist
+      }
+  });
+  res = res.data;
+  trimmedRes.name = res.businesses[0].name;
+  trimmedRes.url = res.businesses[0].url;
+  trimmedRes.address = res.businesses[0].location.address1;
+  trimmedRes.city = res.businesses[0].location.city;
+  console.log(trimmedRes)
     
-
-    try{
-    var trimmedRes = {
-        "name": "",
-        "url": "",
-        "address": "",
-        "city": ""
-    }
-    let res = await axios.get(baseUrl + 'businesses/search', {
-        headers: {
-            Authorization: auth
-        },
-        params: {
-          term: 'thai food',
-          location: 'Texas A&M University',
-          limit: limit,
-          radius: dist
-        }
-    });
-    res = res.data;
-    trimmedRes.name = res.businesses[0].name;
-    trimmedRes.url = res.businesses[0].url;
-    trimmedRes.address = res.businesses[0].location.address1;
-    trimmedRes.city = res.businesses[0].location.city;
-    console.log(trimmedRes)
-    }
-    catch{throw new Error('Failed');}
 }
 
 module.exports = {
